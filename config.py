717ed97551a3351b39e01267d31bae4fe0974b31
shao <qjshao@robvision.cn>
2019-04-17 10:54:32 +0800

    new

--- config.py
+++ config.py
@@ -1,36 +1,45 @@
-#coding:utf-8
+# coding:utf8
 import warnings
+import torch as t
+
 class DefaultConfig(object):
-    env = 'default' # visdom环境
-    model = 'ResNet34' #使用的模型，名字必须和models/__init__.py中的名字一样
+    env = 'default'  # visdom 环境
+    vis_port =8097 # visdom 端口
+    model = 'SqueezeNet'  # 使用的模型，名字必须与models/__init__.py中的名字一致
 
-    train_data_root = './data/train/'
-    test_data_root = './data/test1/'
-    load_model_path = None #加载预训练模型的路径，none表示不加载
+    train_data_root = './data/train/'  # 训练集存放路径
+    test_data_root = './data/test1'  # 测试集存放路径
+    load_model_path = None  # 加载预训练的模型的路径，为None代表不加载
 
-    batch_size = 32
-    use_gpu = True
-    num_workers = 4
-    print_freq = 20
+    batch_size = 32  # batch size
+    use_gpu = True  # user GPU or not
+    num_workers = 4  # how many workers for loading data
+    print_freq = 20  # print info every N batch
 
-    debug_file = '/tmp/debug'
+    debug_file = '/tmp/debug'  # if os.path.exists(debug_file): enter ipdb
     result_file = 'result.csv'
 
     max_epoch = 10
-    lr = 0.001
-    lr_decay = 0.5 # when val_loss increase, lr = lr*lr_decay
-    weight_decay = 0e-5
+    lr = 0.001  # initial learning rate
+    lr_decay = 0.5  # when val_loss increase, lr = lr*lr_decay
+    weight_decay = 0e-5  # 损失函数
 
-def parse(self,kwargs):
-    for k,v in kwargs.items():
-        if not hasattr(self,k):
-            warnings.warn("Warning: opt has not attribut %s" %k)
-        setattr(self,k.v)
 
-    print('user config:')
-    for k,v in self.__class__.__dict__.items():
-        if not k.startwith("__"):
-            print(k,getattr(self, k))
+    def _parse(self, kwargs):
+        """
+        根据字典kwargs 更新 config参数
+        """
+        for k, v in kwargs.items():
+            if not hasattr(self, k):
+                warnings.warn("Warning: opt has not attribut %s" % k)
+            setattr(self, k, v)
+        
+        opt.device =t.device('cuda') if opt.use_gpu else t.device('cpu')
 
-DefaultConfig.parse = parse
+
+        print('user config:')
+        for k, v in self.__class__.__dict__.items():
+            if not k.startswith('_'):
+                print(k, getattr(self, k))
+
 opt = DefaultConfig()
--- main.py
+++ main.py
@@ -1,153 +1,165 @@
-#coding:utf-8
+#coding:utf8
+from config import opt
 import os
 import torch as t
 import models
-from config import opt
 from data.dataset import DogCat
 from torch.utils.data import DataLoader
-from torch.autograd import Variable
 from torchnet import meter
 from utils.visualize import Visualizer
 from tqdm import tqdm
 
 
+@t.no_grad() # pytorch>=0.5
 def test(**kwargs):
-    opt.parse(kwargs)
+    opt._parse(kwargs)
+
+    # configure model
     model = getattr(models, opt.model)().eval()
     if opt.load_model_path:
         model.load(opt.load_model_path)
-    if opt.use_gpu:
-        model.cuda()
+    model.to(opt.device)
 
-    test_data = DogCat(opt.test_data_root, test=True)
-    test_dataloader = DataLoader(test_data, batch_size=opt.batch_size, shuffle=False, num_workers=opt.num_workers)
+    # data
+    train_data = DogCat(opt.test_data_root,test=True)
+    test_dataloader = DataLoader(train_data,batch_size=opt.batch_size,shuffle=False,num_workers=opt.num_workers)
     results = []
-    for ii, (data, path) in tqdm(enumerate(test_dataloader)):
-        input = t.autograd.Variable(data, volatile=True)
-        if opt.use_gpu:
-            input = input.cuda()
+    for ii,(data,path) in tqdm(enumerate(test_dataloader)):
+        input = data.to(opt.device)
         score = model(input)
-        probability = t.nn.functional.softmax(score)[:, 0].data.tolist()
-        # label = score.max(dim = 1)[1].data.tolist()
-
-        batch_results = [(path_, probability_) for path_, probability_ in zip(path, probability)]
+        probability = t.nn.functional.softmax(score,dim=1)[:,0].detach().tolist()
+        # label = score.max(dim = 1)[1].detach().tolist()
+        
+        batch_results = [(path_.item(),probability_) for path_,probability_ in zip(path,probability) ]
 
         results += batch_results
-    write_csv(results, opt.result_file)
+    write_csv(results,opt.result_file)
+
+    return results
 
 def write_csv(results,file_name):
     import csv
-    with open(file_name, 'w') as f:
+    with open(file_name,'w') as f:
         writer = csv.writer(f)
-        writer.writerow(['id', 'label'])
+        writer.writerow(['id','label'])
         writer.writerows(results)
-
+    
 def train(**kwargs):
-    opt.parse(kwargs)
-    vis = Visualizer(opt.env)
+    opt._parse(kwargs)
+    vis = Visualizer(opt.env,port = opt.vis_port)
 
     # step1: configure model
     model = getattr(models, opt.model)()
     if opt.load_model_path:
         model.load(opt.load_model_path)
-    if opt.use_gpu:
-        model.cuda()
+    model.to(opt.device)
 
-    #step2: get data
-    train_data = DogCat(opt.train_data_root, train=True)
-    val_data = DogCat(opt.train_data_root, train=False)
-    train_dataloadet = DataLoader(train_data, batch_size=opt.batch_size, shuffle=True, num_workers=opt.num_workers)
-    val_dataloader = DataLoader(val_data, batch_size=opt.batch_size, shuffle=False, num_workers=opt.num_workers)
-
-    #step3:criterion and optimizer
+    # step2: data
+    train_data = DogCat(opt.train_data_root,train=True)
+    val_data = DogCat(opt.train_data_root,train=False)
+    train_dataloader = DataLoader(train_data,opt.batch_size,
+                        shuffle=True,num_workers=opt.num_workers)
+    val_dataloader = DataLoader(val_data,opt.batch_size,
+                        shuffle=False,num_workers=opt.num_workers)
+    
+    # step3: criterion and optimizer
     criterion = t.nn.CrossEntropyLoss()
     lr = opt.lr
-    optimizer = t.optim.Adam(model.parameters(), lr=lr, weight_decay=opt.weight_decay)
-
-    #step4:meters 一个轻量的计算库
+    optimizer = model.get_optimizer(lr, opt.weight_decay)
+        
+    # step4: meters
     loss_meter = meter.AverageValueMeter()
     confusion_matrix = meter.ConfusionMeter(2)
-    previous_loss = 1e100
+    previous_loss = 1e10
 
-    #train
+    # train
     for epoch in range(opt.max_epoch):
-
+        
         loss_meter.reset()
         confusion_matrix.reset()
 
-        #tqdm:一个Python中进度条加载的工具 不影响实际程序 只是为了美观
-        for ii, (data, label) in tqdm(enumerate(train_dataloadet)):
+        for ii,(data,label) in tqdm(enumerate(train_dataloader)):
 
-            #train model
-            input = Variable(data)
-            target = Variable(label)
+            # train model 
+            input = data.to(opt.device)
+            target = label.to(opt.device)
 
-            #device = t.device("cuda:0" if t.cuda.is_available() else "cpu")
-            #input = input.to(device)
-            #target = target.to(device)
-
-            if opt.use_gpu:
-                input = input.cuda()
-                target = target.cuda()
 
             optimizer.zero_grad()
             score = model(input)
             loss = criterion(score,target)
             loss.backward()
             optimizer.step()
+            
+            
+            # meters update and visualize
+            loss_meter.add(loss.item())
+            # detach 一下更安全保险
+            confusion_matrix.add(score.detach(), target.detach()) 
 
-            #meters updata and Visualize
-            loss_meter.add(loss.data[0])
-            confusion_matrix.add(score.data,target.data)
-
-            if ii%opt.print_freq==opt.print_freq-1:
-                vis.plot('loss',loss_meter.value()[0])
-
+            if (ii + 1)%opt.print_freq == 0:
+                vis.plot('loss', loss_meter.value()[0])
+                
+                # 进入debug模式
                 if os.path.exists(opt.debug_file):
-                    import ipdb  #调试
+                    import ipdb;
                     ipdb.set_trace()
+
 
         model.save()
 
-        # validata and visualize
-        val_cm, val_accuracy = val(model, val_dataloader)
+        # validate and visualize
+        val_cm,val_accuracy = val(model,val_dataloader)
 
-        vis.plot('val_accuracy', val_accuracy)
+        vis.plot('val_accuracy',val_accuracy)
         vis.log("epoch:{epoch},lr:{lr},loss:{loss},train_cm:{train_cm},val_cm:{val_cm}".format(
-            epoch=epoch, loss=loss_meter.value()[0], val_cm=str(val_cm.value()), train_cm=str(confusion_matrix.value()),
-            lr=lr))
-
-        if loss_meter.value()[0] > previous_loss:
-            lr = lr*opt.lr_decay
-            for param_group in optimizer.param_group:
+                    epoch = epoch,loss = loss_meter.value()[0],val_cm = str(val_cm.value()),train_cm=str(confusion_matrix.value()),lr=lr))
+        
+        # update learning rate
+        if loss_meter.value()[0] > previous_loss:          
+            lr = lr * opt.lr_decay
+            # 第二种降低学习率的方法:不会有moment等信息的丢失
+            for param_group in optimizer.param_groups:
                 param_group['lr'] = lr
+        
 
         previous_loss = loss_meter.value()[0]
 
+@t.no_grad()
 def val(model,dataloader):
+    """
+    计算模型在验证集上的准确率等信息
+    """
     model.eval()
     confusion_matrix = meter.ConfusionMeter(2)
-    for ii, data in tqdm(enumerate(dataloader)):
-        input, label = data
-        val_input = Variable(input, volatile=True)
-        val_label = Variable(label.type(t.LongTensor), volatile=True)
-        if opt.use_gpu:
-            val_input.cuda()
-            val_label.cuda()
+    for ii, (val_input, label) in tqdm(enumerate(dataloader)):
+        val_input = val_input.to(opt.device)
         score = model(val_input)
-        confusion_matrix.add(score.data.squeeze(),label.type(t.LongTensor))
+        confusion_matrix.add(score.detach().squeeze(), label.type(t.LongTensor))
 
     model.train()
     cm_value = confusion_matrix.value()
-    accuracy = 100.*(cm_value[0][0] + cm_value[1][1])/(cm_value.sum())
+    accuracy = 100. * (cm_value[0][0] + cm_value[1][1]) / (cm_value.sum())
     return confusion_matrix, accuracy
 
+def help():
+    """
+    打印帮助的信息： python file.py help
+    """
+    
+    print("""
+    usage : python file.py <function> [--args=value]
+    <function> := train | test | help
+    example: 
+            python {0} train --env='env0701' --lr=0.01
+            python {0} test --dataset='path/to/dataset/root/'
+            python {0} help
+    avaiable args:""".format(__file__))
 
+    from inspect import getsource
+    source = (getsource(opt.__class__))
+    print(source)
 
 if __name__=='__main__':
     import fire
     fire.Fire()
-
-
-
-
